23d353784eb19428282a0ed8baca14831a41c16e
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-15 13:07:34 -0600

    Initial Commit

---
+++ SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by Brenda Saavedra  on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
