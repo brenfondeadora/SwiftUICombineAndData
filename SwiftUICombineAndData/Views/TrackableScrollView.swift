0bb6b8e72ea89cad843d62abe33d5cfb58836d3a
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-15 19:57:34 -0600

    feat: add assets and init TrackableScrollView

---
+++ SwiftUICombineAndData/Views/TrackableScrollView.swift
@@ -0,0 +1,14 @@
+//
+//  TrackableScrollView.swift
+//  SwiftUICombineAndData
+//
+//  Created by Brenda Saavedra  on 15/04/23.
+//
+
+import SwiftUI
+
+struct TrackableScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
