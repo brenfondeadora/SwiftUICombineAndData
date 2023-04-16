9003817c1ac8080d8028fe8cb5edd9ace7a4b2dc
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-15 20:16:19 -0600

    feat: add trackableScrollView and added to contentView

--- SwiftUICombineAndData/ContentView.swift
+++ SwiftUICombineAndData/ContentView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var contentOffset = CGFloat(0)
+    
     var body: some View {
         NavigationView {
             ZStack(alignment: .top) {
-                Text("Hello, world!")
-                    .padding()
+                TrackableScrollView(offsetChanged: { offset in
+                    contentOffset = offset.y
+                }) {
+                    Text("Hello, world!")
+                        .padding()
+                }
+                
+                VisualEffectBlur(blurStyle: .systemMaterial)
+                    .opacity(contentOffset < -16 ? 1 : 0)
+                    .animation(.easeIn)
+                    .ignoresSafeArea()
+                    .frame(height: 0)
             }
             .frame(maxHeight: .infinity, alignment: .top)
             .background(AccountBackground())
--- SwiftUICombineAndData/Views/TrackableScrollView.swift
+++ SwiftUICombineAndData/Views/TrackableScrollView.swift
@@ -7,8 +7,33 @@
 
 import SwiftUI
 
-struct TrackableScrollView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct TrackableScrollView<Content: View>: View {
+    let axes: Axis.Set
+    let offesetChanged: (CGPoint) -> Void
+    let content: Content
+    
+    init(axes: Axis.Set = .vertical, offsetChanged: @escaping (CGPoint) -> Void = { _ in }, @ViewBuilder content:() -> Content) {
+        self.axes = axes
+        self.offesetChanged = offsetChanged
+        self.content = content()
     }
+    
+    var body: some View {
+        SwiftUI.ScrollView(axes) {
+            GeometryReader { geometry in
+                Color.clear.preference(key: ScrollOffsetPreferenceKey.self, value: geometry.frame(in: .named("scrollView")).origin)
+            }
+            .frame(width: 0, height: 0)
+            
+            content
+        }
+        .coordinateSpace(name: "scrollView")
+        .onPreferenceChange(ScrollOffsetPreferenceKey.self, perform: offesetChanged)
+    }
+}
+
+private struct ScrollOffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGPoint = .zero
+
+    static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {}
 }
