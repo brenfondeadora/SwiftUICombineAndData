f27746d1f9d3b73aa4f271ee4f8d1cf8826016b9
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-15 20:36:17 -0600

    style: add initial views

---
+++ SwiftUICombineAndData/Views/FAQView.swift
@@ -0,0 +1,20 @@
+//
+//  FAQView.swift
+//  SwiftUICombineAndData
+//
+//  Created by Brenda Saavedra  on 15/04/23.
+//
+
+import SwiftUI
+
+struct FAQView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FAQView_Previews: PreviewProvider {
+    static var previews: some View {
+        FAQView()
+    }
+}
---
+++ SwiftUICombineAndData/Views/PackagesView.swift
@@ -0,0 +1,20 @@
+//
+//  PackagesView.swift
+//  SwiftUICombineAndData
+//
+//  Created by Brenda Saavedra  on 15/04/23.
+//
+
+import SwiftUI
+
+struct PackagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PackagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        PackagesView()
+    }
+}
