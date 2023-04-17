7303e79ddce83177c3c27d10e56516dbd70f8d15
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-17 17:00:52 -0600

    style: add view with text and packagers

--- SwiftUICombineAndData/UI/Screens/PackagesView.swift
+++ SwiftUICombineAndData/UI/Screens/PackagesView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct PackagesView: View {
+    
+    @State private var contentOffset = CGFloat(0)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top) {
+            TrackableScrollView(offsetChanged: { offsetPoint in
+                contentOffset = offsetPoint.y
+            }) {
+                content
+            }
+            
+            VisualEffectBlur(blurStyle: .systemMaterial)
+                .opacity(contentOffset < -16 ? 1 : 0)
+                .animation(.easeIn, value: true)
+                .ignoresSafeArea()
+                .frame(height: 0)
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .navigationTitle("SwiftUI Packages").font(.largeTitle)
+        .background(Color("Background 2").edgesIgnoringSafeArea(.all))
+    }
+    
+    var content: some View {
+        VStack(alignment: .leading, spacing: 16.0) {
+            Text("This app was 100% made using SwiftUI. We’d like to thank these amazing Swift Packages for making our lives as creators easier.")
+                .font(.subheadline)
+                .opacity(0.7)
+                .frame(maxWidth: .infinity, alignment:. leading)
+                .fixedSize(horizontal: false, vertical: true)
+            
+            ForEach(packagesData, id: \.id) { package in
+                PackageRow(package: package)
+            }
+        }
+        .padding(.horizontal, 20)
     }
 }
 
@@ -17,7 +50,11 @@ struct PackagesView: View {
 #if DEBUG
 struct PackagesView_Previews: PreviewProvider {
     static var previews: some View {
-        PackagesView()
+        Group {
+            PackagesView()
+            PackagesView()
+                .environment(\.colorScheme, .dark)
+        }
     }
 }
 #endif
