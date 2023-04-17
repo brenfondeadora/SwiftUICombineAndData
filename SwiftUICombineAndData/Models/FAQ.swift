25550617557f5d4c67427a730aca66dd60ccf06c
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-17 17:09:45 -0600

    feat: create FAQ model

---
+++ SwiftUICombineAndData/Models/FAQ.swift
@@ -0,0 +1,14 @@
+//
+//  FAQ.swift
+//  SwiftUICombineAndData
+//
+//  Created by Brenda Saavedra  on 17/04/23.
+//
+
+import Foundation
+
+struct FAQ: Identifiable, Decodable {
+    var id: Int
+    var question: String
+    var answer: String
+}
