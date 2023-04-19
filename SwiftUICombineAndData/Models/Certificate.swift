b67ee87f13d2fbdb2691ae0a8e91e9f26b47549c
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-19 13:52:55 -0600

    feat: add fetch data from firestore and fill [Certificates]

---
+++ SwiftUICombineAndData/Models/Certificate.swift
@@ -0,0 +1,16 @@
+//
+//  Certificate.swift
+//  SwiftUICombineAndData
+//
+//  Created by Brenda Saavedra  on 19/04/23.
+//
+
+import Foundation
+
+struct Certificate: Identifiable {
+    var id: String
+    var course: String
+    var date: String
+    var logo: String
+    var instructor: String
+}
