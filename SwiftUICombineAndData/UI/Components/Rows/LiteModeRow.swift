6d43a94766a0cab124aab928e90f163b29cc5106
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-20 11:30:12 -0600

    style: create LiteModeRow

---
+++ SwiftUICombineAndData/UI/Components/Rows/LiteModeRow.swift
@@ -0,0 +1,40 @@
+//
+//  LiteModeRow.swift
+//  SwiftUICombineAndData
+//
+//  Created by Brenda Saavedra  on 20/04/23.
+//
+
+import SwiftUI
+
+struct LiteModeRow: View {
+    @State private var subscribed: Bool = true
+    
+    var body: some View {
+        Toggle(isOn: $subscribed, label: {
+            HStack(spacing: 12) {
+                GradientIcon(icon: "speedometer")
+                
+                VStack(alignment: .leading) {
+                    Text("Lite Mode")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                    
+                    Text("Better performance. Recommended for iPhone X and older.")
+                        .font(.caption2)
+                        .opacity(0.7)
+                }
+            }
+        })
+        .toggleStyle(SwitchToggleStyle(tint: Color(#colorLiteral(red: 0.3450980392, green: 0.337254902, blue: 0.8392156863, alpha: 1))))
+    }
+}
+
+// MARK: - Preview
+#if DEBUG
+struct LiteModeRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LiteModeRow()
+    }
+}
+#endif
