d02597c859a3c2216504ee7d5c726e5bdf677aa9
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-25 09:48:11 -0600

    feat: display toggle only if authorized, call getNotificationSettings every time scenePhase is active

--- SwiftUICombineAndData/UI/Components/Rows/NotificationRow.swift
+++ SwiftUICombineAndData/UI/Components/Rows/NotificationRow.swift
@@ -8,28 +8,64 @@
 import SwiftUI
 
 struct NotificationRow: View {
-    @State private var subscribed: Bool = true
+    @StateObject var notificationVM = NotificationViewModel()
+    @Environment(\.scenePhase) private var scenePhase
     
     var body: some View {
-        Toggle(isOn: $subscribed, label: {
-            HStack(spacing: 12) {
-                GradientIcon(icon: "bell.fill")
-                
-                VStack(alignment: .leading) {
-                    Text("Notify me of new content")
+        Group {
+            if notificationVM.permission == .authorized {
+                Toggle(isOn: $notificationVM.subscribedToAllNotifications, label: {
+                    HStack(spacing: 12) {
+                        GradientIcon(icon: "bell.fill")
+                        
+                        VStack(alignment: .leading) {
+                            Text("Notify me of new content")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                            
+                            Text("Max once a week.")
+                                .font(.caption2)
+                                .opacity(0.7)
+                        }
+                    }
+                })
+                .toggleStyle(SwitchToggleStyle(tint: Color(#colorLiteral(red: 0.3450980392, green: 0.337254902, blue: 0.8392156863, alpha: 1))))
+            } else {
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Notifications are disabled")
                         .font(.subheadline)
                         .fontWeight(.semibold)
                     
-                    Text("Max once a week.")
+                    Text("We can't notify you when new content is available.")
                         .font(.caption2)
                         .opacity(0.7)
+                    
+                    Link(destination: URL(string: UIApplication.openSettingsURLString)!, label: {
+                        Text("Open Settings")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                    })
                 }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+            }
+        }
+        .onAppear {
+            notificationVM.getNotificationSettings()
+        }
+        .onChange(of: scenePhase, perform: { newPhase in
+#if DEBUG
+            print("onChange of scenePhase", newPhase)
+#endif
+            if newPhase == .active {
+                notificationVM.getNotificationSettings()
             }
         })
-        .toggleStyle(SwitchToggleStyle(tint: Color(#colorLiteral(red: 0.3450980392, green: 0.337254902, blue: 0.8392156863, alpha: 1))))
-
+        
     }
 }
+
 
 // MARK: - Preview
 #if DEBUG
